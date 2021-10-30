5b5f3edaf5a473ec6ba03c1da27ba81d0ac28316
Jack Keller <jack@freshma.de>
2021-10-30 14:32:34 -0500

    Updates button to use cssvars and adds hover state

--- src/Button.svelte
+++ src/Button.svelte
@@ -1,28 +1,49 @@
 <script lang="ts">
-  export let name: string;
-  export let appearance: string;
-  export let href: string;
-  export let target: string;
+  export let name: 'name';
+  export let appearance: '';
+  export let href: '';
+  export let target: '';
 </script>
 
 <style lang="sass">
+$bg-default: #ededed
+$bg-positive: #009d0e
+$bg-negative: #930
+
 .btn
-  background: #ededed
+  --button-background: #{$bg-default}
+  --button-background-hover: #{darken($bg-default, 20%)}
+  --button-background-positive: #{$bg-positive}
+  --button-background-positive-hover: #{darken($bg-positive, 20%)}
+  --button-background-negative: #{$bg-negative}
+  --button-background-negative-hover: #{darken($bg-negative, 20%)}
+
+  background: var(--button-background)
   border: 1px solid #ccc
   color: #2d2d2d
   font: normal 1rem/1.1 sans-serif
   padding: .5rem 1.25rem
   text-decoration: none
+  transition: all .2s ease-in-out
+
+  &:hover
+    background: var(--button-background-hover)
 
   &.positive
-    background: #009d0e
+    background: var(--button-background-positive)
     border-color: #007d0e
     color: #fff
 
+    &:hover
+      background: var(--button-background-positive-hover)
+
   &.negative
-    background: #930
+    background: var(--button-background-negative)
     border-color: #900
     color: #fff
+
+    &:hover
+      background: var(--button-background-negative-hover)
 </style>
 
 <svelte:options tag="jack-button" />
